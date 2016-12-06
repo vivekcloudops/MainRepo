75f0213e94be2466a1eb369ac35c3b1aae68ba1f
Barange <vivek.barange@accenture.com>
2016-12-06 19:39:24 +0530

    adding src folder from batch file

--- git_commands.bat
+++ git_commands.bat
@@ -4,7 +4,7 @@ git checkout master
 git status
 git add .
 git commit -m "adding src folder from batch file"
-git remote add cloudOps https://github.com/vivekcloudops/MainRepo.git
-git push -u cloudOps master
+git remote add origin https://github.com/vivekcloudops/MainRepo.git
+git push -u origin master
 
 pause
